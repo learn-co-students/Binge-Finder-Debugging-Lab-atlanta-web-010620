8c26c9ed4c1d42eeb9a93295220fb1ba618b0a25
github username <github email address>
2020-03-11 16:03:30 -0400

    Done.

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import {Grid} from 'semantic-ui-react';
+import TVShow from './TVShow'
 
 class TVShowList extends Component {
 
   mapAllShows = () => {
-    if (!!props.searchTerm){
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
+    if (!!this.props.searchTerm){
+      return this.props.shows.map((s) => {
+        if (s.name.toLowerCase().includes(this.props.searchTerm)){
+          return (<TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
         }
       })
     }
-    return props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={props.selectShow}/>)
+    return this.props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
   }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.checkScroll);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.checkScroll)
+  }
+
+  checkScroll = () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+       this.props.handleScroll()
+    }
+ }
 
   render() {
     return (
